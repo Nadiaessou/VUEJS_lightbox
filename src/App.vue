4895e6124b2e8af5192ebb9e9c4415f90a70e921
Nadiaessou <nadia.essoubai@gmail.com>
2021-12-26 21:48:16 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div v-if="title == 'one'">
+        <a :href="'https://picsum.photos/id/' + n + '/800/600'" v-lightbox v-for="n in 9">
+            <img :src="'https://picsum.photos/id/' + n + '/150/150'">
+        </a>
+    </div>
+    <section>
+        <div v-if="title == 'two'">
+            <a :href="'https://picsum.photos/id/' + n + '/800/600?grayscale'" v-lightbox v-for="n in 9">
+                <img :src="'https://picsum.photos/id/' + n + '/150/150?grayscale'">
+            </a>
+        </div>
+    </section>
+    <lightbox></lightbox>
+    <button @click="title = 'one'">ONE</button>
+    <button @click="title = 'two'">TWO</button>
+  </div>
+</template>
+
+<script>
+import Lightbox from './components/lightbox/Lightbox.vue'
+
+export default {
+    data () {
+        return {
+            title: 'one'
+        }
+    },
+  components: {
+      Lightbox
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/lightbox/Lightbox.vue
@@ -0,0 +1,49 @@
+<style src="./lightbox.css"></style>
+
+<template>
+    <div class="lightbox" v-if="image">
+        <transition :name="transition">
+            <lightbox-image :image="image" :key="image"></lightbox-image>
+        </transition>
+        <div class="lightbox__close" @click="close"></div>
+        <div class="lightbox__btn lightbox__next" @click.stop.prevent="next"></div>
+        <div class="lightbox__btn lightbox__prev" @click.stop.prevent="prev"></div>
+    </div>
+</template>
+
+<script>
+import './LightboxDirective'
+import LightboxImage from './LightboxImage.vue'
+import store from './LightboxStore'
+
+    export default {
+        components: { LightboxImage },
+        data() {
+            return {
+                state: store.state,
+                direction: 'next'
+            }
+        },
+        methods: {
+            close () { store.close() },
+            next () {
+                this.direction = 'next'
+                store.next()
+                },
+            prev () {
+                this.direction = 'prev'
+                store.prev()
+                }
+        },
+        computed: {
+            image() {
+                if (this.state.index !== false) {
+                    return this.state.images[this.state.index]
+                    }
+                },
+            transition () {
+                return 'lightbox-' + this.direction
+            }
+        }
+    }
+</script>
